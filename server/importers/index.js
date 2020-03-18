2d7d0462931cd0fef0c7a0d4e46abb1d9d118411
Mitra Ardron <mitra@mitra.biz>
2020-03-18 11:46:17 +1100

    Add comments and pointers to issues to importers/index.js

--- server/importers/index.js
+++ server/importers/index.js
@@ -1,7 +1,12 @@
 // ADD-IMPORTER - add line here to import
+// Data set from israel - its json exported from arcgis
 const israel = require('./israel');
+// Data set from Korea - its a .js file so currently just "required" but that means no live updating see https://github.com/mitra42/corona-tracker/issues/18
 const korea1 = require('./korea1');
+// Data set from Korea - its a CSV file importer could probably be genericized as it starts with parameters in top of CSV https://github.com/mitra42/corona-tracker/issues/25
 const korea2 = require('./korea2');
+// Data set from Diamond Princess visit to Taiwan
+// its KML and importer could probably be genericized https://github.com/mitra42/corona-tracker/issues/26
 const diamondtaiwan = require('./diamondtaiwan');
 
 // ADD-IMPORTER - add import name here
